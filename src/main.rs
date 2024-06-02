34e5b40c382bf6a94acb9611f1150a171bc0b7d8
fabseman <fabse_utilities@pm.me>
2024-06-02 20:26:47 +0200

    Now exits cleanly, means pgo profiling will work

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,10 @@ impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             .add_systems(Startup, add_people)
-            .add_systems(Update, (update_people, greet_people).chain());
+            .add_systems(
+                Update,
+                (update_people, greet_people, bevy::window::close_on_esc).chain(),
+            );
     }
 }
 
@@ -28,7 +31,7 @@ struct GreetTimer(Timer);
 fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
     if timer.0.tick(time.delta()).just_finished() {
         for name in &query {
-            println!("hello {}!", name.0);
+            println!("Hello {}!", name.0);
         }
     }
 }
